034367184f7d20bc1b056669077010dd591837b0
Stephen DiCato <locker537@gmail.com>
2013-03-14 14:57:13 -0400

    Add admin files per each app.

---
+++ trials/apps/exercises/admin.py
@@ -0,0 +1,5 @@
+from trials.apps.exercises.models import Trial, Exercise
+from django.contrib import admin
+
+admin.site.register(Trial)
+admin.site.register(Exercise)
---
+++ trials/apps/people/admin.py
@@ -0,0 +1,5 @@
+from trials.apps.people.models import Student, Teacher
+from django.contrib import admin
+
+admin.site.register(Student)
+admin.site.register(Teacher)
