099f0ac3a08676c4788e844ff256189e42bd85bc
Stephen DiCato <locker537@gmail.com>
2013-04-01 16:22:54 -0400

    Add trials inline for exercises.

--- trials/apps/exercises/admin.py
+++ trials/apps/exercises/admin.py
@@ -1,5 +1,14 @@
 from trials.apps.exercises.models import Trial, Exercise
 from django.contrib import admin
 
+
+class TrialInline(admin.TabularInline):
+    model = Trial
+
+
+class ExerciseAdmin(admin.ModelAdmin):
+    inlines = [TrialInline]
+
+
 admin.site.register(Trial)
-admin.site.register(Exercise)
+admin.site.register(Exercise, ExerciseAdmin)
